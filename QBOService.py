d60f793112f028734035d294c6a473d674649b3d
charan <pvcharankarthikeyan@gmail.com>
2020-09-02 07:04:10 -0500

    Removed unwanted functions

--- QBOService.py
+++ QBOService.py
@@ -6,36 +6,6 @@ from flask import session
 from utils import context, APICallService
 import json
 import config
-# """
-# NOTE: NOT NEEDED and NOT USED
-# """
-# def create_customer(excel_customer, req_context):
-#     """Create a customer object with customer data from a working dictionary"""
-#     full_name = excel_customer['Full Name']
-#     name_list = full_name.split(' ')
-#     first_name = name_list[0]
-#     last_name = name_list[-1]
-#     if len(name_list) > 2:
-#         middle_name = str(name_list[1:len(name_list) - 1])
-#     else:
-#         middle_name = ''
-    
-#     # Create customer object 
-#     customer = {
-#         'GivenName': first_name,
-#         'MiddleName': middle_name,
-#         'FamilyName': last_name,
-#         'PrimaryPhone': {
-#             'FreeFormNumber': excel_customer['Phone']
-#         },
-#         'PrimaryEmailAddr': {
-#             'Address': excel_customer['Email']
-#         }
-#     }
-
-#     uri = '/customer?minorversion=' + config.API_MINORVERSION
-#     response = APICallService.post_request(req_context, uri, customer)
-#     return response
 
 """
 @brief : Function to get the name of the sandbox that the tool is connected to
